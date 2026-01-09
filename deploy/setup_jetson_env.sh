6edc79c4189e5c94a8dccaf6e977df05159b2f56
Mohammad Almasri <m.almasri@al-ghanem.com>
2026-01-10 00:55:34 +0300

    fixed the setup

--- deploy/setup_jetson_env.sh
+++ deploy/setup_jetson_env.sh
@@ -29,18 +29,47 @@ cd build_temp
 # Install gdown to download from Google Drive
 python3.8 -m pip install gdown
 
-# Download PyTorch 1.8.0
-# Source: QEngineering/PyTorch-Jetson-Nano (GitHub Raw)
-echo "Downloading PyTorch 1.8.0..."
-wget https://github.com/Qengineering/PyTorch-Jetson-Nano/raw/main/torch-1.8.0a0%2Bf5911a3-cp38-cp38-linux_aarch64.whl
+# 1.5 Install OpenMPI 4.0.3 (Required for PyTorch 1.12+ on Ubuntu 18.04)
+# JetPack 4.6 (Ubuntu 18.04) has old OpenMPI, so we build the newer version.
+echo "[1.5/5] Building OpenMPI 4.0.3..."
+if [ ! -f "/usr/local/lib/libmpi_cxx.so.40" ]; then
+    wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.3.tar.gz
+    tar xf openmpi-4.0.3.tar.gz
+    cd openmpi-4.0.3
+    ./configure --prefix=/usr/local
+    make -j4
+    sudo make install
+    sudo ldconfig
+    cd ..
+    rm -rf openmpi-4.0.3 openmpi-4.0.3.tar.gz
+else
+    echo "OpenMPI 4.0.3 seems to be installed."
+fi
+
+# 2. Install PyTorch & Torchvision for Jetson Nano (Python 3.8 + aarch64)
+# Source: QEngineering/PyTorch-Jetson-Nano (Hosted on Google Drive)
+echo "[2/5] Downloading and installing PyTorch 1.12.0 & Torchvision 0.13.0..."
+mkdir -p build_temp
+cd build_temp
+
+# Install gdown to download from Google Drive
+python3.8 -m pip install gdown
+
+# Download PyTorch 1.12.0
+# ID: 1MnVB7I4N8iVDAkogJO76CiQ2KRbyXH_e -> torch-1.12.0a0+...
+echo "Downloading PyTorch 1.12.0..."
+gdown 1MnVB7I4N8iVDAkogJO76CiQ2KRbyXH_e
 TORCH_WHEEL=$(find . -maxdepth 1 -name "torch*.whl" | head -n 1)
 python3.8 -m pip install "$TORCH_WHEEL"
 
-# Download Torchvision 0.9.0
-# Source: QEngineering/PyTorch-Jetson-Nano (GitHub Raw)
-echo "Downloading Torchvision 0.9.0..."
-# Note: Torchvision 0.9.0 corresponds to PyTorch 1.8.0
-wget https://github.com/Qengineering/PyTorch-Jetson-Nano/raw/main/torchvision-0.9.0a0%2B01dfa8e-cp38-cp38-linux_aarch64.whl
+# Download Torchvision 0.13.0
+# We need 0.13.0 for PyTorch 1.12.0. The previous ID was for 0.14/1.13.
+# Let's find the ID for 0.13.0.
+# From the subagent output in Step 75, I recall seeing 0.13.0.
+# Link: https://drive.google.com/file/d/11DPKcWzLjZa5kRXRodRJ3t9md0EMydhj/view?usp=sharing
+# ID: 11DPKcWzLjZa5kRXRodRJ3t9md0EMydhj
+echo "Downloading Torchvision 0.13.0..."
+gdown 11DPKcWzLjZa5kRXRodRJ3t9md0EMydhj
 VISION_WHEEL=$(find . -maxdepth 1 -name "torchvision*.whl" | head -n 1)
 python3.8 -m pip install "$VISION_WHEEL"
 
